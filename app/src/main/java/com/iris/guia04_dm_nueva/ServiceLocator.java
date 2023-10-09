7ecb581f99ea02696c4387d35c7217c488cc3dc8
iris936 <iriscalderon936@gmail.com>
2023-10-08 19:30:44 -0600

    Proyecto Finalizado

---
+++ app/src/main/java/com/iris/guia04_dm_nueva/ServiceLocator.java
@@ -0,0 +1,26 @@
+package com.iris.guia04_dm_nueva;
+
+public class ServiceLocator {
+    private static ServiceLocator instance = null;
+    private static RepositorioTrabajador mainRepositoryinstance = null;
+
+    private ServiceLocator() {}
+
+    public static ServiceLocator getInstance() {
+        if (instance == null) {
+            synchronized(ServiceLocator.class) {
+                instance = new ServiceLocator();
+            }
+        }
+        return instance;
+    }
+
+    public RepositorioTrabajador getDBSource() {
+        if (mainRepositoryinstance == null) {
+            synchronized(ServiceLocator.class) {
+                mainRepositoryinstance = new RepositorioTrabajador();
+            }
+        }
+        return mainRepositoryinstance;
+    }
+}
